f75cda4fff1cc38516517d23af43421c3490eadf
Jan Philipp Tebernum <jan.tebernum@pm.me>
2024-10-24 22:00:36 +0200

    fix: various fixes and clean ups for entity allocations/deletions

--- src/entity.rs
+++ src/entity.rs
@@ -56,43 +56,34 @@ impl Entities {
     /// Allocate a new entity.
     ///
     /// The closure `f` needs to use the newly created [`Entity`] and use it for further
-    /// allocations in [`Archetype`]s and [`Table`]s.
-    pub fn alloc(
+    /// allocations in [`Archetype`]s and [`Table`]s. After the allocation is used, the caller
+    /// needs to provide the [`EntityLocation`] of the entity.
+    pub(crate) fn alloc(
         &mut self,
         f: impl FnOnce(Entity) -> Result<EntityLocation, ()>,
     ) -> Result<Entity, ()> {
         if let Some(EntityEntry { entry, generation }) = self.entities.get_mut(self.free_head) {
             if let Entry::Free { next_free } = entry {
                 let entity = Entity::from(self.free_head as u32, *generation);
-                match f(entity) {
-                    Ok(loc) => {
-                        self.free_head = *next_free;
-                        *entry = Entry::Occupied { loc };
-                        self.len += 1;
-                        return Ok(entity);
-                    }
-                    // The usage of the newly created entity failed, so we ignore it and do nothing
-                    // about it here. Ideally the implementor of `f` should handle the error.
-                    Err(_) => (),
+                if let Ok(loc) = f(entity) {
+                    self.free_head = *next_free;
+                    *entry = Entry::Occupied { loc };
+                    self.len += 1;
+                    return Ok(entity);
                 }
             } else {
                 panic!("Entities free list is corrupt, failed to allocate entity!");
             }
         } else {
             let entity = Entity::from(self.entities.len() as u32, 0);
-            match f(entity) {
-                Ok(loc) => {
-                    self.entities.push(EntityEntry {
-                        generation: 0,
-                        entry: Entry::Occupied { loc },
-                    });
-                    self.free_head = self.entities.len();
-                    self.len += 1;
-                    return Ok(entity);
-                }
-                // The usage of the newly created entity failed, so we ignore it and do nothing
-                // about it here. Ideally the implementor of `f` should handle the error.
-                Err(_) => (),
+            if let Ok(loc) = f(entity) {
+                self.entities.push(EntityEntry {
+                    generation: 0,
+                    entry: Entry::Occupied { loc },
+                });
+                self.free_head = self.entities.len();
+                self.len += 1;
+                return Ok(entity);
             }
         }
 
@@ -115,13 +106,15 @@ impl Entities {
         if let Some(EntityEntry { entry, generation }) =
             self.entities.get_mut(entity.index as usize)
         {
-            if let Entry::Occupied { .. } = entry {
-                *generation += 1;
-                *entry = Entry::Free {
-                    next_free: self.free_head,
-                };
-                self.free_head = entity.index as usize;
-                self.len -= 1;
+            if *generation == entity.generation {
+                if let Entry::Occupied { .. } = entry {
+                    *generation += 1;
+                    *entry = Entry::Free {
+                        next_free: self.free_head,
+                    };
+                    self.free_head = entity.index as usize;
+                    self.len -= 1;
+                }
             }
         }
     }
@@ -189,6 +182,46 @@ mod tests {
                 archetype_id: ArchetypeId(1),
                 table_id: TableId(1),
                 table_row: TableRow(0),
+            })
+        );
+    }
+
+    #[test]
+    fn double_free() {
+        let mut entities = Entities::new();
+        let entity1 = entities
+            .alloc(|entity| {
+                Ok(EntityLocation {
+                    archetype_id: ArchetypeId(0),
+                    table_id: TableId(0),
+                    table_row: TableRow(0),
+                })
+            })
+            .unwrap();
+
+        entities.free(entity1);
+
+        let entity2 = entities
+            .alloc(|entity| {
+                Ok(EntityLocation {
+                    archetype_id: ArchetypeId(0),
+                    table_id: TableId(0),
+                    table_row: TableRow(1),
+                })
+            })
+            .unwrap();
+
+        assert_eq!(entity1.index, entity2.index);
+        assert_ne!(entity1.generation, entity2.generation);
+
+        entities.free(entity1);
+        assert_eq!(entities.len, 1);
+        assert_eq!(
+            entities.get(entity2),
+            Some(&EntityLocation {
+                archetype_id: ArchetypeId(0),
+                table_id: TableId(0),
+                table_row: TableRow(1)
             })
         );
     }
